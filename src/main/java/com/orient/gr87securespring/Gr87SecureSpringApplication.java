a718962a2849ed279bbcbf0615928370a4596c49
NarminQ <quliyevanermin05012001@gmail.com>
2023-04-08 15:03:06 +0400

    first commit

---
+++ src/main/java/com/orient/gr87securespring/Gr87SecureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.orient.gr87securespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gr87SecureSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gr87SecureSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orient/gr87securespring/Gr87SecureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orient.gr87securespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gr87SecureSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
